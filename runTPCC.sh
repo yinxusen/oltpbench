994528fee3f834a5f8bf8b248fff9a0bb1e8e8fe
Xusen Yin <yinxusen@gmail.com>
2016-04-09 19:12:22 -0700

    finally I know how to use, why no readme?

--- runTPCC.sh
+++ runTPCC.sh
@@ -1,2 +1,2 @@
-java -Xmx1024m -cp `run/classpath.sh` -Dlog4j.configuration=log4j.properties com.oltpbenchmark.DBWorkload -b tpcc -s 1 -c config/sample_tpcc_config.xml -o tpcc_result -d tpcc_report --dialects-export true --execute true
+java -Xmx1024m -cp `run/classpath.sh` -Dlog4j.configuration=log4j.properties com.oltpbenchmark.DBWorkload -b tpcc -s 1 -c config/sample_tpcc_config.xml -o tpcc_result -d tpcc_report --clear true --create true --load true --execute true
 
